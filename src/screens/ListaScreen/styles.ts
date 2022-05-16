94867d539a75fe74464f287ae6b2bd84f1b13304
larissaschottz <80956083+larissaschottz@users.noreply.github.com>
2022-05-15 21:11:09 -0300

    Add files via upload

---
+++ src/screens/ListaScreen/styles.ts
@@ -0,0 +1,43 @@
+import styled, { css } from "styled-components/native";
+
+export const Container = styled.View`
+${({theme}) => css`
+    background: ${theme.colors.background};
+    flex: 1;
+    position: relative;
+  `}
+`;
+
+export const ContainerBackgroundImage = styled.ImageBackground`
+  width: 100%;
+  height: 200px;
+
+  position: absolute;
+  top: 0px;
+`;
+
+export const ScrollView = styled.ScrollView`
+  padding: 0 20px;
+`;
+
+export const Content = styled.View`
+  margin-bottom: 50px;
+`;
+
+export const Title = styled.Text`
+${({theme}) => css`
+    color: ${theme.colors.textBlack};
+    font-size: 32px;
+    font-weight: bold;
+    margin-top: 80px;
+  `}
+`;
+
+export const Paragraph = styled.Text`
+${({theme}) => css`
+    color: ${theme.colors.textGray};
+    font-size: 16px;
+    margin: 10px 0;
+    font-weight: 400;
+  `}
+`;
