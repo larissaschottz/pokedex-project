fefc526b6eb56b72569014f142d8e753844e0257
larissaschottz <80956083+larissaschottz@users.noreply.github.com>
2022-05-15 21:08:06 -0300

    Delete src/components/global/styles directory

--- src/components/global/styles/styled.t.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components'
-//importando o tema criado
-import theme from './theme'
-declare module 'styled-components' {
-    type ThemeType = typeof theme
-
-    export interface DefaultTheme extends ThemeType {}
-}
-//criando referenciamento dos estilos criados no tema (tipagem dinâmica)
