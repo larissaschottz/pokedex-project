cc68ac83a43dcb76d8e601a48b52656f494ac117
larissaschottz <80956083+larissaschottz@users.noreply.github.com>
2022-05-15 21:07:50 -0300

    Add files via upload

---
+++ src/components/global/styles/theme.ts
@@ -0,0 +1,13 @@
+export default {
+    colors: {
+        background: '#FFFFFF',
+        backgroundWater: '#58ABF6',
+        textWhite: '#FFFFFF',
+        textBlack: '#58575F',
+        textGray: '#9DA0AA',
+
+    types: {
+        water: '#4A90DA',
+    }
+    }
+}
